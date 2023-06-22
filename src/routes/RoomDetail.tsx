3fe8d676d78c2a81c16ed2c84dab6168f9303502
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2023-06-22 23:11:15 +0900

    css 🎨: room detail

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -2,9 +2,55 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { getRoom } from '../api';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Skeleton,
+} from '@chakra-ui/react';
+import { IRoomDetail } from '../type';
 
 export default function RoomDetail() {
   const { roomPk } = useParams();
-  const { data: room } = useQuery(['rooms', roomPk], getRoom);
-  return <div>room detail</div>;
+  const { isLoading, data: room } = useQuery<IRoomDetail>(
+    ['rooms', roomPk],
+    getRoom
+  );
+  return (
+    <Box mt={10} px={{ base: '10', lg: '48' }}>
+      <Skeleton h={10} w={'100%'} isLoaded={!isLoading}>
+        <Heading fontSize={'3xl'}>{room?.name}</Heading>
+      </Skeleton>
+      <Grid
+        mt={8}
+        rounded="xl"
+        overflow={'hidden'}
+        gap={2}
+        height="60vh"
+        templateRows={'1fr 1fr'}
+        templateColumns={'repeat(4, 1fr)'}
+      >
+        {[0, 1, 2, 3, 4].map((index) => (
+          <GridItem
+            colSpan={index === 0 ? 2 : 1}
+            rowSpan={index === 0 ? 2 : 1}
+            overflow={'hidden'}
+            key={index}
+          >
+            <Skeleton isLoaded={!isLoading} h="100%" w="100%">
+              <Image
+                objectFit={'cover'}
+                w="100%"
+                h="100%"
+                bgColor={'gray.200'}
+                src={room?.photos[index]?.file}
+              />
+            </Skeleton>
+          </GridItem>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
