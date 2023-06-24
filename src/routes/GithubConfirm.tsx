e4f47961f6bb471feb08a3e988bf6c942ab601d5
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2023-06-25 07:53:40 +0900

    refactor ✨: social login confirm screen

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -2,33 +2,38 @@ import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { githubLogIn } from '../api';
 import { Heading, Spinner, Text, VStack, useToast } from '@chakra-ui/react';
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function GithubConfirm() {
   const { search } = useLocation();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const toast = useToast();
+
+  const mutation = useMutation(githubLogIn, {
+    onSuccess: () => {
+      queryClient.refetchQueries(['me']);
+      navigate('/');
+      toast({
+        status: 'success',
+        title: 'Log in',
+        description: '다시 봬서 반가워요!',
+        position: 'bottom-right',
+      });
+    },
+  });
+
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get('code');
     if (code) {
-      const status = await githubLogIn(code);
-      if (status === 200) {
-        queryClient.refetchQueries(['me']);
-        navigate('/');
-        toast({
-          status: 'success',
-          title: 'Log in',
-          description: '다시 봬서 반가워요!',
-          position: 'bottom-right',
-        });
-      }
+      mutation.mutate(code);
     }
   };
   useEffect(() => {
     confirmLogin();
   }, []);
+
   return (
     <VStack justifyContent={'center'} mt={40}>
       <Heading>Processing log in...</Heading>
--- src/routes/KakaoConfirm.tsx
+++ src/routes/KakaoConfirm.tsx
@@ -2,28 +2,32 @@ import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { kakaoLogIn } from '../api';
 import { Heading, Spinner, Text, VStack, useToast } from '@chakra-ui/react';
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function KakaoConfirm() {
   const { search } = useLocation();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const toast = useToast();
+
+  const mutation = useMutation(kakaoLogIn, {
+    onSuccess: () => {
+      queryClient.refetchQueries(['me']);
+      navigate('/');
+      toast({
+        status: 'success',
+        title: 'Log in',
+        description: '다시 봬서 반가워요!',
+        position: 'bottom-right',
+      });
+    },
+  });
+
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get('code');
     if (code) {
-      const status = await kakaoLogIn(code);
-      if (status === 200) {
-        queryClient.refetchQueries(['me']);
-        navigate('/');
-        toast({
-          status: 'success',
-          title: 'Log in',
-          description: '다시 봬서 반가워요!',
-          position: 'bottom-right',
-        });
-      }
+      mutation.mutate(code);
     }
   };
   useEffect(() => {
